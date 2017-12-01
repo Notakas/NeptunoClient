4da65b3eee8ba96ca2f18074e4c56d9deaab90db
Notakas <notakas.jap@gmail.com>
2017-12-01 18:55:44 +0100

    Otro commit

---
+++ app/controllers.js
@@ -0,0 +1,17 @@
+var app = angular.module('neptunoApp', []);
+
+app.controller('CtrlLogin', ['$scope', function ($scope) {
+
+}]);
+
+app.controller('CtrlHola', ['$scope', function ($scope) {
+
+}]);
+
+app.controller('CtrlListaCliente', ['$scope', function ($scope) {
+
+}]);
+
+app.controller('CtrlGuardarCliente', ['$scope', function ($scope) {
+
+}]);
