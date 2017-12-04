447fdeb0bbcfff95503037dd483af17b63afd2fb
Notakas <notakas.jap@gmail.com>
2017-12-04 19:51:20 +0100

    A

--- app/components/cliente/cliente.js
+++ app/components/cliente/cliente.js
@@ -1,21 +1,26 @@
 var app = angular.module('neptunoApp');
-app.controller('CtrlListaCliente', ['$scope', '$http', function ($scope, $http) {
-     $scope.listaCliente = [];
-     $scope.submit = function() {
-         var promise = $http.post('listaCliente', []);
-         promise.then(function(data, status, headers, config) {
-         $scope.listaCliente = data.data;
-         }), function(error) {
-         alert( "Error: " + JSON.stringify({error: error}));
-         };
-     };
+app.controller('CtrlListaCliente', ['$scope', '$http', function ($scope, $http, clienteService) {
+    $scope.listaCliente = [];
+    var promise = $http.post('listaCliente', []);
+    promise.then(function (data, status, headers, config) {
+        $scope.listaCliente = data.data;
+    }), function (error) {
+        alert("Error: " + JSON.stringify({ error: error }));
+    };
 }]);
 
 app.controller('CtrlGuardarCliente', ['$scope', '$http', function ($scope, $http) {
-    var cliente = {
+    var promise = $http.post('http://localhost:8080/TiendaNeptuno/verCliente/1', []);
+    promise.then(function (data, status, headers, config) {
+        
+    }), function (error) {
+        alert("Error: " + JSON.stringify({ error: error }));
+    };
+
+    cliente = {
         "cif": $scope.cifCliente,
         "nombreEmpresa": $scope.nombreEmpresaCliente,
-        "nombrePersona": $scope.nombrePersonaCliente,
+        "nombrePersona": $scope.nombrePersonaCliente ? 1 : 0,
         "direccion": $scope.direccionCliente,
         "codigoPostal": $scope.codigoPostalCliente,
         "pais": $scope.paisCliente,
