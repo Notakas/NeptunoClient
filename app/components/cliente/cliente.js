b24021ae0bbec63568bf8ca5f30b35854f4054bd
Notakas <notakas.jap@gmail.com>
2017-12-04 16:59:26 +0100

    GuardarCliente y empleado estaban revertidos

--- app/components/cliente/cliente.js
+++ app/components/cliente/cliente.js
@@ -1,12 +1,22 @@
 var app = angular.module('neptunoApp');
 app.controller('CtrlListaCliente', ['$scope', '$http', function ($scope, $http) {
-     $scope.listaCliente = [];
-     $scope.submit = function() {
-         var promise = $http.post('listaCliente', []);
-         promise.then(function(data, status, headers, config) {
-         $scope.listaCliente = data.data;
-         }), function(error) {
-         alert( "Error: " + JSON.stringify({error: error}));
-         };
-     };
+    $scope.listaCliente = [];
+    var promise = $http.post('listaCliente', []);
+    promise.then(function (data, status, headers, config) {
+        $scope.listaCliente = data.data;
+    }), function (error) {
+        alert("Error: " + JSON.stringify({ error: error }));
+    };
+}]);
+
+app.controller('CtrlGuardarCliente', ['$scope', '$http', function ($scope, $http) {
+    $scope.listaCliente = [];
+    $scope.submit = function () {
+        var promise = $http.post('listaCliente', []);
+        promise.then(function (data, status, headers, config) {
+            $scope.listaCliente = data.data;
+        }), function (error) {
+            alert("Error: " + JSON.stringify({ error: error }));
+        };
+    };
 }]);
