ac2aaab3e9d70d310db28d96bc308ba073a86109
EnriqueBerdiales <enriqueberdiales@gmail.com>
2017-12-05 15:51:50 +0100

    Revert "controladores de empleado y producto finalizados"
    This reverts commit 12f7f9aa1de24091f7aa2b0b8051ebaf2c974834.

--- app/components/producto/producto.js
+++ app/components/producto/producto.js
@@ -1,7 +1,7 @@
 var app = angular.module('neptunoApp');
 app.controller('CtrlListaProducto', ['$scope', '$http', function ($scope, $http) {
      $scope.listaProducto = [];
-         var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/listaProducto', []);
+         var promise = $http.post('listaProducto', []);
          promise.then(function(data, status, headers, config) {
          $scope.listaProducto = data.data;
          }), function(error) {
@@ -9,38 +9,16 @@ app.controller('CtrlListaProducto', ['$scope', '$http', function ($scope, $http)
          };
 }]);
 
-app.controller('CtrlGuardarProducto', ['$scope', '$http','$routeParams', function ($scope, $http, $routeParams) {
-
-    var promise = $http.get('http://192.168.43.73:8081/TiendaNeptuno/verProducto/'+$routeParams.id);
-
-    promise.then(function(data, status, headers, config) {
-        producto = data.data;
-        $scope.nombreProducto=producto.nombreProducto;
-        $scope.idProducto=producto.idProducto;
-        $scope.descripcionProducto=producto.descripcion;
-        $scope.precioVentaProducto=producto.precioVenta;
-        $scope.precioCompraProducto=producto.precioCompraProducto;
-        $scope.existenciasProducto=producto.existenciasProducto;
-    }), function(error) {
-        alert( "Error: " + JSON.stringify({error: error}));
-    };
+app.controller('CtrlGuardarProducto', ['$scope', '$http', function ($scope, $http) {
+    var Producto={"nombreProducto":$scope.nombreProducto,"idProducto":$scope.idProducto,
+    "descripcion":$scope.descripcionProducto,"precioVenta":$scope.precioVentaProducto,
+    "precioCompra":$scope.precioCompraProducto,"existencias":$scope.existenciasProducto
+}
     $scope.guardarProducto = function () {
-
-        var producto=new Object();
-
-        producto.nombreProducto;
-        producto.idProducto;
-        producto.descripcionProducto;
-        producto.precioVentaProducto;
-        producto.precioCompraProducto;
-        producto.existenciasProducto;
-        if (producto.idProducto!=null)
-            var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/updateProducto',producto);
-        else
-            var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/addProducto',producto);
-            promise.then(function (data, status, headers, config) {
-            }), function (error) {
-            alert("Error: " + JSON.stringify({ error: error }));
-        };
+    var promise = $http.post('http://localhost:8080/AngularSpring/guardarProducto',Producto);
+    promise.then(function (data, status, headers, config) {   
+    }), function (error) {
+        alert("Error: " + JSON.stringify({ error: error }));
     };
+};
 }]);
