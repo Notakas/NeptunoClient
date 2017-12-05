07602171e1a14a394e0f2ae2db48ff7678045ea1
Notakas <notakas.jap@gmail.com>
2017-12-05 18:09:00 +0100

    A??adido login

---
+++ app/components/login/js/appLogin.js
@@ -0,0 +1,18 @@
+var app = angular.module("neptunoApp");
+app.controller("CtrlLogin",[ '$scope', '$http', function($scope, $http){
+	$scope.login = function() { 
+	
+	var data = {
+				"dniEmpleado":$scope.dniEmpleado,
+				"contrasena":$scope.contrasena
+				}
+
+	
+		var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno', data); 
+		promise.then(function(data, status, headers, config) { 
+			$scope.mensaje=data.data.Mensaje;
+			}), function(error) {     
+				alert( "Error: " + JSON.stringify({error: error}));  
+		};
+	  };
+}]);
