9168fdfe69c093ff75431f594956de8df7d40f1b
EnriqueBerdiales <enriqueberdiales@gmail.com>
2017-12-05 15:57:02 +0100

    controladores de empleado y producto finalizados2

--- app/components/empleado/empleado.js
+++ app/components/empleado/empleado.js
@@ -24,3 +24,56 @@ app.controller('CtrlGuardarEmpleado', ['$scope', '$http', function ($scope, $htt
     };
 };
 }]);
+
+var app = angular.module('neptunoApp');
+app.controller('CtrlListaProducto', ['$scope', '$http', function ($scope, $http) {
+     $scope.listaProducto = [];
+         var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/listaProducto', []);
+         promise.then(function(data, status, headers, config) {
+         $scope.listaProducto = data.data;
+         }), function(error) {
+         alert( "Error: " + JSON.stringify({error: error}));
+         };
+}]);
+
+app.controller('CtrlGuardarEmpleado', ['$scope', '$http','$routeParams', function ($scope, $http, $routeParams) {
+
+    var promise = $http.get('http://192.168.43.73:8081/TiendaNeptuno/verEmpleado/'+$routeParams.id);
+
+    promise.then(function(data, status, headers, config) {
+        empleado = data.data;
+        $scope.dni=empleado.dni;
+        $scope.nombreEmpleado=empleado.nombreEmpleado;
+        $scope.apellidos=empleado.apellidos;
+        $scope.cargo=empleado.cargo;
+        $scope.direccion=empleado.direccion;
+        $scope.ciudad=empleado.ciudad;
+        $scope.codigoPostal=empleado.codigoPostal;
+        $scope.pais=empleado.pais;
+        $scope.telefono=empleado.telefono;
+    }), function(error) {
+        alert( "Error: " + JSON.stringify({error: error}));
+    };
+    $scope.guardarEmpleado = function () {
+
+        var empleado=new Object();
+
+        empleado.dni;
+        empleado.nombreEmpleado;
+        empleado.apellidos;
+        empleado.cargo;
+        empleado.direccion;
+        empleado.ciudad;
+        empleado.codigoPostal;
+        empleado.pais;
+        empleado.telefono;
+        if (empleado.idEmpleado!=null)
+            var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/updateEmpleado',empleado);
+        else
+            var promise = $http.post('http://192.168.43.73:8081/TiendaNeptuno/addEmpleado',empleado);
+            promise.then(function (data, status, headers, config) {
+            }), function (error) {
+            alert("Error: " + JSON.stringify({ error: error }));
+        };
+    };
+}]);
